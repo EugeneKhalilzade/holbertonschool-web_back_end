ba44c253a5b893a99eb8f6db00fe7efd8cf301c4
EugeneKhalilzade <elgynkhalilzade@gmail.com>
2026-02-12 20:40:42 +0000

    .

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,34 +1,26 @@
 #!/usr/bin/env python3
 """
-Module for zooming (duplicating) elements of a sequence.
+Module that provides a function to zoom (duplicate)
+elements of a tuple.
 """
 
-from typing import List, Tuple, TypeVar
-
-T = TypeVar('T')
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[T, ...], factor: int = 2) -> List[T]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Returns a list where each element of the input tuple
     is repeated `factor` times.
 
     Args:
-        lst (Tuple[T, ...]): A tuple of elements of any type.
+        lst (Tuple): A tuple of elements.
         factor (int): Number of times each element is repeated.
 
     Returns:
-        List[T]: A new list with duplicated elements.
+        List: A list containing duplicated elements.
     """
-    zoomed_in: List[T] = [
+    zoomed_in: List = [
         item for item in lst
         for _ in range(factor)
     ]
     return zoomed_in
-
-
-array: Tuple[int, ...] = (12, 72, 91)
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)  # must be int
